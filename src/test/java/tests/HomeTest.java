17f2ac4f7d0c8d6b5773b56652f1d2c3cf472fd4
pradeeppatil87700 <pradeeppatil87700@gmail.com>
2024-12-02 22:02:33 +0530

    Update HomeTest.java

--- src/test/java/tests/HomeTest.java
+++ src/test/java/tests/HomeTest.java
@@ -6,7 +6,7 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
+   
 public class HomeTest extends BasePage {
     private WebDriver driver;
 
